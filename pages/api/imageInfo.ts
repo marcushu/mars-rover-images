78f2a92fdb78e3876b3501f2708f7dce1845520c
marcushu <marcushurn@hotmail.com>
2021-03-30 15:57:27 -0700

    added cors

--- pages/api/imageInfo.ts
+++ pages/api/imageInfo.ts
@@ -1,7 +1,26 @@
 import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors';
+
+const cors = Cors({
+    methods: ['POST']
+});
+
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
+    return new Promise((resolve, reject) => {
+      fn(req, res, (result: any) => {
+        if (result instanceof Error) {
+          console.error("middleware error: " + result);
+        }
+  
+        return resolve(result)
+      })
+    })
+  }
 
 export default async function imageInfo(req: NextApiRequest, res: NextApiResponse) {
+    await runMiddleware(req, res, cors);
+
     const { current_rover, _page, _sol } = JSON.parse(req.body);
     
     const ans = await axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${current_rover}/photos?page=${_page}&sol=${_sol}&api_key=${process.env.NASA_API_KEY}`);
